2f1bfd71d602dd3be8c82c8f1c3b16fddee80319
impul <pavlo.bojkoo@gmail.com>
2019-11-19 11:51:23 +0200

    Initial Commit

---
+++ Calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calendar
+//
+//  Created by Pavlo Boiko on 11/19/19.
+//  Copyright © 2019 Pavlo Boiko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
